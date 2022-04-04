ea84d17835706fe8e6e97c538e92160be6285c54
MysteriousMango123 <94985982+MysteriousMango123@users.noreply.github.com>
2022-04-04 15:48:31 -0400

    Add files via upload

---
+++ level2/platformer/character/hearts.gd
@@ -0,0 +1,29 @@
+extends Node2D
+var max_health = 3
+var health = max_health
+
+func _process(delta):
+	if health > max_health:
+		$TextureRect.margin_right = (max_health * 54)
+		
+# Called when the node enters the scene tree for the first time.
+	
+func _ready():
+	$TextureRect.margin_right = (max_health * 54)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+		
+	$TextureRect2.margin_right = (max_health * 54)
+		
+	
+func heal_water():
+	health += 1
+	$TextureRect.margin_right = (health * 54)	
+	
+func losehealth(health_lost):
+	health -= health_lost
+	$TextureRect.margin_right = (health * 54)
+
+func heal():
+	health = max_health
+	$TextureRect.margin_right = (health * 54)
