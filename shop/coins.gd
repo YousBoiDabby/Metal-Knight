66f0ddfe61db88204905a628191b7fcfa0f0c00c
MysteriousMango123 <94985982+MysteriousMango123@users.noreply.github.com>
2022-04-09 13:54:48 -0400

    Delete shop directory

--- shop/coins.gd
+++
@@ -1,6 +0,0 @@
-extends Label
-
-func _process(delta):
-	text = ('coins: '+ String(Global.coins))
-
-
--- shop/shop.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-
-func _on_x_pressed():
-	get_tree().change_scene("res://Inside_castle/walls, map/Map.tscn")
